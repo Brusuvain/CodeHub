2b07f57055ba31a96e51f1ff9d6cac5a07ce9d40
Brusuvain <42177208+Brusuvain@users.noreply.github.com>
2021-08-12 15:08:39 -0700

    Add files via upload

--- rotate/src/rotate/mergeKList.java
+++ rotate/src/rotate/mergeKList.java
@@ -2,71 +2,66 @@ package rotate;
 
 import java.util.*;
 
-public class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode() {
-	}
-
-	ListNode(int val) {
-		this.val = val;
-	}
-
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
-	}
-}
-
 public class mergeKList {
 	/**
 	 * Definition for singly-linked list. public class ListNode { int val; ListNode
 	 * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
 	 * ListNode next) { this.val = val; this.next = next; } }
 	 */
-	class Solution {
-		public ListNode mergeKLists(ListNode[] lists) {
-			ListNode head = new ListNode();
-			List<Integer> huh = new ArrayList<>();
-			ListNode headCopy = head;
-			ListNode temp = null;
-			int breakOff = lists.length;
-			int minI = -1;
-			while (breakOff > 0) {
-				ListNode min = new ListNode(10001);
-				for (int i = 0; i < lists.length; i++) {
-					if (huh.contains(i))
-						continue;
-					if (breakOff == 0) {
-						break;
-					}
-					if (lists[i] == null) {
+	public class ListNode {
+		int val;
+		ListNode next;
 
-						// System.out.println(breakOff);
-						if (!huh.contains(i)) {
-							huh.add(i);
-							--breakOff;
-						}
-						continue;
-					}
-					// System.out.print(" "+lists[i].val+" mv is "+min.val);
-					if (lists[i].val <= min.val) {
-						min = lists[i];
-						minI = i;
+		ListNode() {
+		}
 
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		ListNode head = new ListNode();
+		List<Integer> nullIndexList = new ArrayList<>();
+		ListNode headCopy = head;
+		int noOfRemainingsNonNulls = lists.length;
+		int minI = -1;
+		while (noOfRemainingsNonNulls > 0) {
+			// continue iterations till each LinkedList reaches null
+			ListNode min = new ListNode(10001);
+			for (int i = 0; i < lists.length; i++) {
+				if (nullIndexList.contains(i))
+					continue;
+				if (lists[i] == null) {
+					// Only decrease NonNulls counter first time a LinkedList reaches null
+					if (!nullIndexList.contains(i)) {
+						nullIndexList.add(i);
+						--noOfRemainingsNonNulls;
 					}
+					continue;
+				}
+				// finding the minimum value node
+				if (lists[i].val <= min.val) {
+					min = lists[i];
+					minI = i;
 
 				}
-				if (breakOff == 0)
-					break;
-				head.next = new ListNode();
-				head.next.val = min.val;
-				head = head.next;
-				if (lists[minI] != null)
-					lists[minI] = lists[minI].next;
+
 			}
-			return headCopy.next;
+			// break if all nodes have been iterated over
+			if (noOfRemainingsNonNulls == 0)
+				break;
+			head.next = new ListNode();
+			head.next.val = min.val;
+			head = head.next;
+			if (lists[minI] != null)
+				lists[minI] = lists[minI].next;
 		}
+		return headCopy.next;
 	}
 }
